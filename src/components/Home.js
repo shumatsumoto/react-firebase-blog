1425ab50f1c3770def0a14559ca697a5e19d269b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-06 22:51:20 +0900

    Firestoreからデータ取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firabase";
 import "./Home.css";
 
 const Home = () => {
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
       <div className="postContents">
