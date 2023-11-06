271f2e89e508e347dfeef8bafb32dbc3751dee2c
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-11-06 22:12:11 +0900

    Homeコンポーネント作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextConteiner">
+        テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト
+        テキスト テキスト テキスト テキスト
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@shumatsumoto</h3>
+        <button>削除</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
